c1ee57b03af74c88484b1947bb3350585f8f5975
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-09-28 19:53:41 -0300

    Requisito 3 completo - scrape botão próxima página

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,7 +29,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_button = selector.css('.nav-links .next::attr(href)').get()
+    return next_page_button
 
 
 # Requisito 4
